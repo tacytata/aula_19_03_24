5f68872740a6da87186cdd4b1a240e170b075871
Professor Marcelo <avalia.apb2023@gmail.com>
2024-03-11 14:44:35 -0300

    instalar chalk

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+
+console.log(chalk.blue('Olá mundo'));
+
+console.log(chalk.blue.bgWhite.bold("SENAI"));
+
+console.log(chalk.blue('Curso',' de',' Node.JS'));
+
+console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
+
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
