b98961029237714c244250415f09c293acf695ea
Professor Marcelo <avalia.apb2023@gmail.com>
2024-03-14 15:28:54 -0300

    inserir regex

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ import fs from "fs";
 
 import chalk from 'chalk';
 
+
+
+const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extraiLinks (texto){
+    const regex =/\[[^[\]]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)/gm;
+
+    const capturas=texto.match(regex);
+
+    console.log(capturas);
+}
+
+extraiLinks(textoTeste);
+
 // console.log(chalk.blue('Olá mundo'));
 
 // console.log(chalk.blue.bgWhite.bold("SENAI"));
@@ -49,18 +63,18 @@ function trataErro(erro){
 
 //-----------usando outra forma de solucionar as promessas (async/await)
 
-async function pegaArquivo(caminhoDoArquivo){
-    try{
-        const encoding ="utf-8";
-        const texto= await fs.promises.readFile(caminhoDoArquivo,encoding);
-        console.log(chalk.cyan(texto));
-    } catch (erro) {
-        trataErro(erro);
-    } finally {
-        console.log(chalk.magenta('Operação concluída'));
-    }
+// async function pegaArquivo(caminhoDoArquivo){
+//     try{
+//         const encoding ="utf-8";
+//         const texto= await fs.promises.readFile(caminhoDoArquivo,encoding);
+//         console.log(chalk.cyan(texto));
+//     } catch (erro) {
+//         trataErro(erro);
+//     } finally {
+//         console.log(chalk.magenta('Operação concluída'));
+//     }
 
-}
+// }
 
-pegaArquivo('./arquivos/texto.md');
+// pegaArquivo('./arquivos/texto.md');
 //pegaArquivo('./arquivos/');
