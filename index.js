7cdd0d4619ffce14984eff7e14d8b914d85e41dc
Professor Marcelo <avalia.apb2023@gmail.com>
2024-03-15 15:16:11 -0300

    criar vetor nome:valor

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ import chalk from 'chalk';
 const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
 
 function extraiLinks (texto){
-    const regex =/\[[^[\]]*?\]\(https?:\/\/[^\s?#.]*[^\s]*\)/gm;
+    const regex =/\[([^[\]]*?)\]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm;
 
-    const capturas=texto.match(regex);
+    //const capturas=texto.match(regex);
+    //const capturas=regex.exec(texto);
+    //const capturas=texto.matchAll(regex);
 
-    console.log(capturas);
+    //operador de espalhamento
+    const capturas=[...texto.matchAll(regex)];
+
+    const resultados=capturas.map(captura=>({[captura[1]]:captura[2]}));
+
+
+    //console.log(capturas);
+    console.log(resultados);
 }
 
 extraiLinks(textoTeste);
